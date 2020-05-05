6d851519f11dc71e53b187bf835c3176e581ba38
Ericles Piana <ericlespiana@hotmail.com>
2020-05-05 19:16:36 -0300

    Some comments

--- Codes/Matriz_Game.c
+++ Codes/Matriz_Game.c
@@ -1,3 +1,5 @@
+//Esse code funciona como uma espécie de jogo que fiz nos meus estudos de matrizes
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
@@ -8,25 +10,25 @@ int main(void){
 	char mov;
 	
 	printf("Tamanho da matriz: ");
-	scanf("%d",&tam);
+	scanf("%d",&tam); //determinando o tamanho da matriz
 	
-	char m[tam][tam];
+	char m[tam][tam]; //algo legal que a matriz e feita em tempo de execução
 	
 	for(l=0;l<tam;l++)
 		{
 			for(c=0;c<tam;c++)
 			{
-				m[l][c] = '-';
+				m[l][c] = '-'; //preenche toda a matriz com um unico caracter
 			}
 		}
 
-		m[tam-1][0] = 'O';
+		m[tam-1][0] = 'O'; //coloca o 'personagem' na ultima linha da primeira coluna
 
 		for(l=0;l<tam;l++)
 		{
 			for(c=0;c<tam;c++)
 			{
-				printf("%4c",m[l][c]);
+				printf("%4c",m[l][c]); //mostra toda a matriz junto com o personage 'O'
 			}
 			printf("\n");
 		}
@@ -36,20 +38,21 @@ int main(void){
 	do
 	{	
 		
-		mov = getch();
+		mov = getch(); //mov vai receber um caracter do teclado sem eco
 		system("cls");
 		
 		for(l=0;l<tam;l++)
 		{
 			for(c=0;c<tam;c++)
 			{
-				if(m[l][c] == 'O')
+				if(m[l][c] == 'O') //caso a posição encontrado possua o 'O' entra no switch
 				{				
 					switch(mov)
 					{
-						case 'W':
+						case 'W': /*caso W a posição que continha o '0' vai receber o '-' e a posição acima
+						recebe o o '0' caso esteja dentro da matriz. O mesmo vale para todos os outros case*/
 						case 'w':
-								if(l!=0)
+								if(l!=0)//teste para saber se esta dentro da matriz
 								{	
 									m[l][c] = '-';
 									m[l-1][c] = 'O';	
@@ -59,7 +62,7 @@ int main(void){
 				
 						case 'S':
 						case 's':
-								if(l!=tam-1)
+								if(l!=tam-1)//teste para saber se esta dentro da matriz
 								{
 									m[l][c] = '-';
 									m[l+1][c] = 'O';
@@ -69,7 +72,7 @@ int main(void){
 		
 						case 'A':
 						case 'a':
-								if(c!=0)
+								if(c!=0)//teste para saber se esta dentro da matriz
 								{
 									m[l][c] = '-';
 									m[l][c] = '-';
@@ -80,7 +83,7 @@ int main(void){
 						
 						case 'D':
 						case 'd':
-								if(c!=tam-1)
+								if(c!=tam-1)//teste para saber se esta dentro da matriz
 								{
 									m[l][c] = '-';
 									m[l][c] = '-';
@@ -90,7 +93,7 @@ int main(void){
 						break;
 						
 						case 'q':
-								printf("Sair? [Q]");
+								printf("Sair? [Q]"); //caso a tecla for digitado seja 'q', entra nesse case e /mostra a msg
 								scanf("%c",&mov);
 						break;
 					}
@@ -98,7 +101,7 @@ int main(void){
 			}
 		}
 		
-		for(l=0;l<tam;l++)
+		for(l=0;l<tam;l++) //atualiza a matriz depois da tecla ser pressionada
 		{
 			for(c=0;c<tam;c++)
 			{
@@ -107,7 +110,7 @@ int main(void){
 		printf("\n");
 		}
 		
-	}while(mov != 'Q');
+	}while(mov != 'Q');//Q para sair
 
 	return (0);
 }
